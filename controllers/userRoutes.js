16756b6d0475b822199a35252d37d6e30404cf12
devWeibel <weibel.jason@gmail.com>
2022-10-21 15:11:56 -0400

    finished routes maybe?

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 const { User } = require('../models');
+const Task = require('../models/Task');
 
-router.get('/', (req, res) => {
-    res.json('TESTING FROM USER-ROUTES');
+router.get('/', async (req, res) => {
+    // res.json('TESTING FROM USER-ROUTES');
+    try {
+        const userData = await User.findAll({include: [{model: Task}]});
+        res.status(200).json({userData});
+    } catch (err) {
+        res.status(400).json({ msg: 'something went wrong'});
+    }
 });
 
 router.post('/', async (req, res) => {
