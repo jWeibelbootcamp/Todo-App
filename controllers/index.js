3a1b39dfdc89819e13b1cb183c30becdfe06ff21
devWeibel <weibel.jason@gmail.com>
2022-10-21 14:32:23 -0400

    creating user and task routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,2 +1,15 @@
 const router = require('express').Router();
+const taskRoutes = require('./taskRoutes');
+const userRoutes = require('./userRoutes');
 
+router.get('/index', (req, res) => {
+    res.json('hewwo');
+});
+
+//localhost: 5500/api/user
+router.use('/user', userRoutes);
+//localhost: 5500/api/task
+router.use('/task', taskRoutes);
+
+
+module.exports = router;
